64f772649944c8e4a8c63785852ca42324a13fdd
AlonArm <alon.armoni@campus.technion.ac.il>
2023-04-20 18:01:01 +0300

    fixed IsraeliQueueCreateFunction

--- israeliQueue.c
+++ israeliQueue.c
@@ -1,29 +1,32 @@
 #include <stdio.h>      
 #include <stdlib.h>     
-#include "tools/israeliqueue.h"
+#include "IsraeliQueue.h"
 
 //creating the nodes of people in our list with the relevant values
-typedef struct Person 
-{
-int friendsPassed;
-int enemyHeldBack;
+typedef struct person {
+  int friendsPassed;
+  int enemyHeldBack;
 struct Person* next;
 } Person;
 
-struct IsraeliQueue
+struct IsraeliQueue_t
 {
- person* head;
- int friendshipCounter=0; //for realloc and free
-   FriendshipFunction* friendshipFunctions; 
-   ComparisonFunction compare;
- int friendship_th;
- int rivalry_th;
+  struct person* head;
+  int friendshipCounter; //for realloc and free
+  FriendshipFunction* friendshipFunctions; 
+  ComparisonFunction compare;
+  int friendship_th;
+  int rivalry_th;
 
+};
+IsraeliQueue IsraeliQueueCreate(FriendshipFunction* friendFunctions, ComparisonFunction compFunction, int friendThres, int rivalTres){
+  struct IsraeliQueue_t *queue = (struct IsraeliQueue_t*)malloc(sizeof(struct IsraeliQueue_t));
+  queue->friendshipFunctions = friendFunctions;
+  queue->compare = compFunction;
+  queue->friendship_th = friendThres;
+  queue->rivalry_th = rivalTres;
+  return queue;
 }
-IsraeliQueue IsraeliQueueClone(IsraeliQueue q)
-{
 
-
-}
 
 
