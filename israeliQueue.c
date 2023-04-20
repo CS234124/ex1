993fc623d994f62614481e26a1922a83d281ab4b
AlonArm <alon.armoni@campus.technion.ac.il>
2023-04-20 21:46:19 +0300

    changed functions array to a list

--- israeliQueue.c
+++ israeliQueue.c
@@ -9,11 +9,17 @@ typedef struct person {
 struct person* next;
 } person;
 
+struct funcNode{
+  FriendshipFunction func;
+  struct funcNode *next;
+};
+
 struct IsraeliQueue_t
 {
   struct person* head;
-  int friendshipCounter; //for realloc and free
-  FriendshipFunction* friendshipFunctions; 
+  struct funcNode* funcList;
+  //int friendshipCounter; //for realloc and free
+  //FriendshipFunction* friendshipFunctions;
   ComparisonFunction compare;
   int friendship_th;
   int rivalry_th;
@@ -21,10 +27,20 @@ struct IsraeliQueue_t
 };
 IsraeliQueue IsraeliQueueCreate(FriendshipFunction* friendFunctions, ComparisonFunction compFunction, int friendThres, int rivalTres){
   IsraeliQueue queue = (IsraeliQueue)malloc(sizeof(IsraeliQueue));
-  queue->friendshipFunctions = friendFunctions;
+  //queue->friendshipFunctions = friendFunctions;
   queue->compare = compFunction;
   queue->friendship_th = friendThres;
   queue->rivalry_th = rivalTres;
+  if(friendFunctions[0] != NULL){
+    queue->funcList = (struct funcNode*)(malloc(sizeof(struct funcNode)));
+    queue->funcList->func = friendFunctions[0];
+    struct funcNode *temp = queue->funcList;
+    for(int i = 1 ; friendFunctions[i] != NULL ; i++){
+      temp->next = (struct funcNode*)(malloc(sizeof(struct funcNode)));
+      temp->next->func = friendFunctions[i];
+      temp = temp->next;
+    }
+  }
   return queue;
 }
 void IsraeliQueueDestroy(IsraeliQueue queue){
@@ -32,6 +48,12 @@ void IsraeliQueueDestroy(IsraeliQueue queue){
   while(p != NULL){
     struct person* temp = p;
     p = p->next;
+    free(temp);
+  }
+  struct funcNode* f = queue->funcList;
+  while(f != NULL){
+    struct funcNode* temp = f;
+    f = f->next;
     free(temp);
   }
 }
